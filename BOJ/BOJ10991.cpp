fbad3aad8e48894f7cebf20b45b5f85e78306916
JeJune Lee <81547780+LeeJejune@users.noreply.github.com>
2021-07-28 17:32:23 +0900

    BOJ10991
    BOJ10991

---
+++ BOJ/BOJ10991.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+   
+	for (int i = 1; i <= n; i++) {
+		for (int j = i; j <= n - 1; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 1; j <= i * 2 - 1; j++)
+		{
+			if (j % 2 == 1)
+				cout << "*";
+			else
+				cout << " ";
+		}
+			cout << "\n";
+		}
+
+	return 0;
+	}
